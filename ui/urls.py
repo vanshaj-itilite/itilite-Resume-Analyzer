dcfbede59b89747ed79751946f8ece8eeb2b2c50
Vanshaj Kerni <Vanshaj.Kerni@itilite.in>
2026-02-26 19:07:43 +0530

    updated the ui

--- ui/urls.py
+++ ui/urls.py
@@ -9,7 +9,7 @@ app_name = "ui"
 urlpatterns = [
     path("", views.index, name="index"),
     path("analytics/", views.analytics, name="analytics"),
-    path("technical-docs/", views.technical_docs, name="technical_docs"),
+    path("technical_docs/", views.technical_docs, name="technical_docs"),
     path("how_to_use/", views.how_to_use, name="how_to_use"),
 ]
 
